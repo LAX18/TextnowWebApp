875498b795d67c5a773da97c848099f769f3eaca
LAX18 <34464552+LAX18@users.noreply.github.com>
2019-05-10 19:21:03 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function onLoad() {
+  if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+  // Registration was successful
+  console.log('Registered!');
+  }, function(err) {
+  // registration failed :(
+  console.log('ServiceWorker registration failed: ', err);
+  }).catch(function(err) {
+  console.log(err);
+  });
+  });
+  } else {
+  console.log('service worker is not supported');
+  }
+self.addEventListener('install', function() {
+  console.log('Install!');
+});
+self.addEventListener("activate", event => {
+  console.log('Activate!');
+});
+self.addEventListener('fetch', function(event) {
+  console.log('Fetch!', event.request);
+});
+}
